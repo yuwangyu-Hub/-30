ca1109b59497c67a6d1308c78000d1f6531d81b4
yu wangyu <695182937@qq.com>
2021-12-21 18:06:19 +0800

    2021-12-21
    开始建立

--- 极夜30/Person.cs
+++ 极夜30/Person.cs
@@ -6,8 +6,22 @@ namespace 极夜30
 {
     class Person
     {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param 名字="name"></param>
+        /// <param 性别="gender"></param>
+        /// <param 年龄="age"></param>
+        public Person(string name,char gender,int age)
+        {
+            this.Name = name;
+            this.Gender = gender;
+            this.Age = age;
+
+        }
+
         //姓名
-        private string _name;
+        string _name;
         public string Name 
         {
             get {  return _name; }
@@ -15,7 +29,7 @@ namespace 极夜30
         }
 
         //性别
-        private char _gender;
+        char _gender;
         public char Gender 
         {
             get { return _gender; }
@@ -23,15 +37,44 @@ namespace 极夜30
         }
         
         //年龄
-        private int _age;
+        int _age;
         public int Age
         {
             get { return _age; }
             set { _age = value; }
         }
 
+        //身高
+        float _high;
+        public float High
+        {
+            get { return _high; }
+            set { _high = value; }
+        }
 
+        //长相（一到十分）
+        int _looks;
+        public int Looks
+        {
+            get { return _looks; }
+            set {
+                if (value < 0 || value > 10)
+                    _looks = 5;//如果长相小于0或者大于10，那么长相就给5，表示中等
+                else
+                    _looks = value;
+            }
+        }
+
+        //健康状况（1-10）
+        //知识（1-10）
+        //力气（1-10）
+        //职业
+        //情绪（1-10）
         //
+        //--------------战斗相关
+        //武器熟练度（1-10）
+        //
+
 
     }
 }
