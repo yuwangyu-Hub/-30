518b8efa4fa687a0b4f2a8fdef6e5bd23eb863e2
yu wangyu <695182937@qq.com>
2021-12-21 17:03:26 +0800

    上传测试

--- 极夜30/Person.cs
+++ 极夜30/Person.cs
@@ -8,10 +8,29 @@ namespace 极夜30
     {
         //姓名
         private string _name;
+        public string Name 
+        {
+            get {  return _name; }
+            set {  _name = value; }
+        }
+
         //性别
         private char _gender;
+        public char Gender 
+        {
+            get { return _gender; }
+            set { _gender = value; } 
+        }
+        
         //年龄
         private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+
         //
 
     }
