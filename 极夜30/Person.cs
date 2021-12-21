1bc52471272c19c3156749d9717d6fed80bda863
yu wangyu <695182937@qq.com>
2021-12-21 16:55:32 +0800

    Initial commit

---
+++ 极夜30/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 极夜30
+{
+    class Person
+    {
+        //姓名
+        private string _name;
+        //性别
+        private char _gender;
+        //年龄
+        private int _age;
+        //
+
+    }
+}
---
+++ 极夜30/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 极夜30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
